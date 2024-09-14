ea4e77e09707ad5981fbe721db55a586dd9b7558
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-09-14 11:47:03 +0500

    animation added

--- App.tsx
+++ App.tsx
@@ -1,28 +1,46 @@
 import React, { PropsWithChildren, useEffect, useRef } from "react";
-import { Animated, SafeAreaView, Text, View, ViewStyle } from "react-native";
+import { Animated, Easing, SafeAreaView, Text, View, ViewStyle } from "react-native";
 
 
 type FadeInViewProps = PropsWithChildren<{ style: ViewStyle }>;
 
 const FadeInView: React.FC<FadeInViewProps> = props => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
+  const translateAnim = useRef(new Animated.Value(300)).current;
 
   useEffect(() => {
     Animated.timing(
       fadeAnim,
       {
         toValue: 1,
-        duration: 2000,
+        duration: 5000,
         useNativeDriver: true,
       }
     ).start();
   }, [fadeAnim])
+
+  useEffect(() => {
+    Animated.timing(
+      translateAnim,
+      {
+        toValue: 0,
+        easing: Easing.back(),
+        duration: 5000,
+        useNativeDriver: true,
+      }
+    ).start();
+  }, [translateAnim])
 
   return (
     <Animated.View
       style={{
         ...props.style,
         opacity: fadeAnim,
+        transform: [
+          { translateY: translateAnim }
+        ],
+        borderRadius: 10,
+        elevation: 5,
       }}
     >
       {props.children}
@@ -37,9 +55,9 @@ const App = () => {
       <View>
         <FadeInView style={{ backgroundColor: 'powderblue', padding: 20 }}>
           <Text>Fade In View</Text>
-          <Text>Welcome to the React App</Text>
-          <Text>This is a simple React app.</Text>
         </FadeInView>
+        <Text>Welcome to the React App</Text>
+        <Text>This is a simple React app.</Text>
       </View>
     </SafeAreaView>
   );
